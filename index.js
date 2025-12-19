a0308363315e3240a015615d4a7159bcc748dcc5
Maryam Bashir <155750306+MariyamBashir@users.noreply.github.com>
2025-12-19 16:51:39 +0500

    Refactor cart server to use path module for file

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 console.log("maryam");
 const express = require("express");
 const fs = require ("fs");
+const path = require("path");
 
 const app = express();
 app.use(express.json());
 
-const CART_FILE = "cart.json";
+const CART_FILE = path.join(__dirname,"cart.json");
 
 app.get("/cart", (req, res) => {
   const data = fs.readFileSync(CART_FILE, "utf-8");
@@ -35,6 +36,7 @@ app.post("/cart", (req, res) => {
 app.listen(3000, () => {
   console.log("Cart server running on port 3000");
 });
+
 
 
 
