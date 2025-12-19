ed53491e52168d6243d2f21575cb1727910deebf
Maryam Bashir <155750306+MariyamBashir@users.noreply.github.com>
2025-12-19 15:23:41 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+console.log("maryam");
+const express = require("express");
+const fs = require ("fs");
+
+const app = express();
+app.use(express.json());
+
+const CART_FILE = "./cart.json";
+
+app.get("/cart", (req, res) => {
+  const data = fs.readFileSync(CART_FILE, "utf-8");
+  res.json(JSON.parse(data));
+});
+
+app.post("/cart", (req, res) => {
+  const cart = JSON.parse(fs.readFileSync(CART_FILE));
+
+  const newItem = {
+    id: Date.now(),
+    productId: req.body.productId,
+    productName: req.body.productName,
+    price: req.body.price,
+    quantity: req.body.quantity
+  };
+
+  cart.push(newItem);
+  fs.writeFileSync(CART_FILE, JSON.stringify(cart, null, 2));
+
+  res.status(201).json({
+    message: "Item added to cart",
+    item: newItem
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Cart server running on port 3000");
+});
+
+
